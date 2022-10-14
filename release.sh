73e479aafbe85a45a870db8b16a33d3c7fb5d3c1
Anders Jaensson <anders.jaensson@smartbear.com>
2022-10-14 08:38:53 +0200

    chore: disable changelog modification, change scm urls

--- release.sh
+++ release.sh
@@ -3,7 +3,7 @@
 set -euxo pipefail
 
 : "${1?"Usage: $0 <[pre]major|[pre]minor|[pre]patch|prerelease>"}"
-: "${CHANGELOG_GITHUB_TOKEN?"Needs CHANGELOG_GITHUB_TOKEN env var"}"
+#: "${CHANGELOG_GITHUB_TOKEN?"Needs CHANGELOG_GITHUB_TOKEN env var"}"
 
 ./mvnw scm:check-local-modification
 
@@ -15,13 +15,13 @@ git checkout -b "release/${release}"
 
 ./mvnw versions:set -D newVersion="${release}"
 git commit -am "Release ${release}"
-./mvnw clean deploy scm:tag -P release -D tag="${release}" -D pushChanges=false -D skipTests -D dependency-check.skip
+./mvnw clean install scm:tag -P release -D tag="${release}" -D pushChanges=false -D skipTests -D dependency-check.skip
 
 git push --tags
 
 ./mvnw versions:set -D newVersion="${next}-SNAPSHOT"
-docker run -it --rm -e CHANGELOG_GITHUB_TOKEN -v "$(pwd)":/usr/local/src/your-app \
-    githubchangeloggenerator/github-changelog-generator -u zalando -p logbook
+#docker run -it --rm -e CHANGELOG_GITHUB_TOKEN -v "$(pwd)":/usr/local/src/your-app \
+#    githubchangeloggenerator/github-changelog-generator -u zalando -p logbook
 git commit -am "Development ${next}-SNAPSHOT"
 
 git push
